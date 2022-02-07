bab43d233c1d5ed5860bf1891817868ab7ace3ae
DESKTOP-DF467M0\Money Maker <dionisisloug@gmail.com>
2022-02-07 13:00:53 +0200

    Complete commit of simple calculator app using external web service

---
+++ Page1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Page1.aspx.cs" Inherits="Page1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="Layout.css">
+    <title>Calculator</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label class="labels" ID="titleLabel" runat="server" Text="Calculator"></asp:Label>
+        <br />
+        <asp:Label class="labels" ID="firstNumLabel" runat="server" Text="First Number: "></asp:Label>&nbsp;&nbsp; <asp:TextBox ID="firstInput" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="addButton" runat="server" Text="" Height="69px" Width="76px" OnClick="addButton_Click" />&nbsp; <asp:Button ID="subtractButtton" runat="server" Text="" Height="69px" Width="76px" OnClick="subtractButtton_Click" />&nbsp; <asp:Button ID="multiplyButton" runat="server" Text="" Height="69px" Width="76px" OnClick="multiplyButton_Click" />&nbsp; <asp:Button ID="divideButton" runat="server" Text="" Height="69px" Width="76px" OnClick="divideButton_Click" />&nbsp; <asp:Button ID="clearButton" runat="server" Text="" Height="75px" Width="75px" OnClick="clearButton_Click" />&nbsp;
+        <br />
+        <asp:Label class="labels" ID="secondNumLabel" runat="server" Text="Second Number :"></asp:Label>&nbsp; <asp:TextBox ID="secondInput" runat="server" Width="144px"></asp:TextBox>
+        <br />
+        <asp:Label class="labels" ID="resultLabel" runat="server" Text="Result:"></asp:Label>&nbsp;<asp:Label class="labels" ID="resultOutputLabel" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Page1.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using com.dneonline.www;
+
+
+public partial class Page1 : System.Web.UI.Page
+{
+    Calculator myWs = new Calculator();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void addButton_Click(object sender, EventArgs e)
+    {
+        if (firstInput.Text != "" && secondInput.Text != "")
+        {
+            int firstNum, secondNum, result;
+            firstNum = Convert.ToInt32(firstInput.Text);
+            secondNum = Convert.ToInt32(secondInput.Text);
+            result = myWs.Add(firstNum, secondNum);
+            resultOutputLabel.Text = Convert.ToString(result);
+        }
+        else
+        {
+            resultOutputLabel.Text = "Please enter First and Second Number and try again!";
+        }
+    }
+
+    protected void subtractButtton_Click(object sender, EventArgs e)
+    {
+        if (firstInput.Text != "" && secondInput.Text != "")
+        {
+            int firstNum, secondNum, result;
+            firstNum = Convert.ToInt32(firstInput.Text);
+            secondNum = Convert.ToInt32(secondInput.Text);
+            result = myWs.Subtract(firstNum, secondNum);
+            resultOutputLabel.Text = Convert.ToString(result);
+        }
+        else
+        {
+            resultOutputLabel.Text = "Please enter First and Second Number and try again!";
+        }
+    }
+
+    protected void multiplyButton_Click(object sender, EventArgs e)
+    {
+        if (firstInput.Text != "" && secondInput.Text != "")
+        {
+            int firstNum, secondNum, result;
+            firstNum = Convert.ToInt32(firstInput.Text);
+            secondNum = Convert.ToInt32(secondInput.Text);
+            result = myWs.Multiply(firstNum, secondNum);
+            resultOutputLabel.Text = Convert.ToString(result);
+        }
+        else
+        {
+            resultOutputLabel.Text = "Please enter First and Second Number and try again!";
+        }
+    }
+
+    protected void divideButton_Click(object sender, EventArgs e)
+    {
+        if (firstInput.Text != "" && secondInput.Text != "")
+        {
+            int firstNum, secondNum, result;
+            firstNum = Convert.ToInt32(firstInput.Text);
+            secondNum = Convert.ToInt32(secondInput.Text);
+            if (secondNum != 0)
+            {
+                result = myWs.Divide(firstNum, secondNum);
+                resultOutputLabel.Text = Convert.ToString(result);
+            }
+            else
+            {
+                resultOutputLabel.Text = "You can't divide by zero..";
+            }
+        }
+        else
+        {
+            resultOutputLabel.Text = "Please enter First and Second Number and try again!";
+        }
+    }
+
+    protected void clearButton_Click(object sender, EventArgs e)
+    {
+        firstInput.Text = "";
+        secondInput.Text = "";
+        resultOutputLabel.Text = "";
+    }
+}
